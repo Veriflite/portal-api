fa16d57669a1f71635b091ee2db3b47471305f36
John Howe <john@veriflite.com>
2023-02-01 15:21:18 +1300

    gen script to create markdown and html

---
+++ asyncapi/gen.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+ag asyncapi/portal-api.yaml @asyncapi/markdown-template --force-write
+mv asyncapi.md README.md
+ag asyncapi/portal-api.yaml @asyncapi/html-template --output html --force-write
